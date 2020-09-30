02b3c559a2b4fa41e8defb749d9b0c7b06dbf4dd
Muskan Kalsi <31847576+muskan09@users.noreply.github.com>
2020-09-30 23:32:01 +0530

    Update tkinter_text_editor.py

--- tkinter_text_editor.py
+++ tkinter_text_editor.py
@@ -34,17 +34,17 @@ window.title('Text Editor')
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-fr_buttons = tk.Frame(master = window, relief=tk.RAISED, borderwidth=1)
+fr_buttons = tk.Frame(master = window, relief=tk.RAISED, borderwidth=1, relief = tk.RAISED, borderwidth=5)
 
-btn_open = tk.Button(master = fr_buttons, text='Open', command = open_file)
-btn_save = tk.Button(master = fr_buttons, text = 'Save As', command = save_file)
+btn_open = tk.Button(master = fr_buttons, text='Open', command = open_file, bg='DeepSkyBlue')
+btn_save = tk.Button(master = fr_buttons, text = 'Save As', command = save_file, bg='DeepSkyBlue')
 
 btn_open.grid(row=0, column= 0, sticky='ew', padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky='ew', padx=5)
 
 fr_buttons.grid(row=0, column=0, sticky='ns')
 
-txt_edit = tk.Text(master = window)
+txt_edit = tk.Text(master = window, bg='cornflower blue', borderwidth=5, relief=tk.RAISED)
 txt_edit.grid(row=0, column=1, sticky='nsew')
 
 window.mainloop()
